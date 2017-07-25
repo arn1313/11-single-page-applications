2481841e996544edcb0d0dcf8e83a6e317f8b1e7
Aaron McPeek <arn1313@comcast.net>
2017-07-25 10:58:27 -0700

    done with todo's

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,12 +1,11 @@
 'use strict';
 var app = app || {};
 
-page('/index', articleView.initArticleView)
-page('/new', newArticlePage.initNewArticlePage)
-page('/admin', adminView.initadminView)
+page('/', articleController.init)
+page('/about', aboutController.init)
 
 
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
+// DONE: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
 
-// TODONE: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
+// DONE: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
 page()
